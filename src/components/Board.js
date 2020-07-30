d4344d1c42504dd2110c51dfce26549f628adb1f
zeromvx <zeromvx@gmail.com>
2020-07-30 13:22:50 +1000

    Added changing moves

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,12 +5,23 @@ import Square from './Square';
 class Board extends Component {
     state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
     }
 
     handleClick = (i) => {
         const squares = [...this.state.squares];
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        const { xIsNext } = this.state;
+        
+        if ( squares[i] || calculateWinner(squares)) {
+            return;
+        }
+
+        xIsNext ? squares[i] = 'X': squares[i] = 'O';
+
+        this.setState({ 
+            squares: squares,
+            xIsNext: !xIsNext,
+        });
         
     }
 
@@ -21,33 +32,64 @@ class Board extends Component {
                 onClick={ () => this.handleClick(i) }
                 />
         );
+        
     }
 
     render() {
-
+        const winner = calculateWinner(this.state.squares);
         let status;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
                 <div className="status">{ status }</div>
                 <div className="board__row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                    { this.renderSquare(0) }
+                    { this.renderSquare(1) }
+                    { this.renderSquare(2) }
                 </div>
                 <div className="board__row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
+                    { this.renderSquare(3) }
+                    { this.renderSquare(4) }
+                    { this.renderSquare(5) }
                 </div>
                 <div className="board__row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
+                    { this.renderSquare(6) }
+                    { this.renderSquare(7) }
+                    { this.renderSquare(8) }
                 </div>
             </div>
         )
     }
 }
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
+}
+
 
 export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Square.css';
 
 const Square = ({ value, onClick }) => (
-    <button className="square" onClick={ () => onClick() }>
+    <button className="square" onClick={ onClick }>
         { value }
     </button>
 )
